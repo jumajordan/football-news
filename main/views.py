431919d7e8f0081f960be061d1edb323dda66b56
jumajordan <jumajordan2506@gmail.com>
2025-09-10 15:16:41 +0700

    push lagi

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A'
+        'npm' : '24016435843',
+        'name': 'Juma Jordan Bimo Simnjuntak',
+        'class': 'PBP F'
     }
 
     return render(request, "main.html", context)
