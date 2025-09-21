c24a4527d8662c8d6fb15be452435f6855d9b0ea
Jumaannjii <135129916+Jumaannjii@users.noreply.github.com>
2025-09-21 23:02:20 +0700

    push

--- main/urls.py
+++ main/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<str:news_id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
+    
 ]
